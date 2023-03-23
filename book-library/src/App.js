c2beb0002339176a21efea3f51cfcc82696aff25
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-03-23 21:50:06 +0000

    Switches from Express to Firebase

--- book-library/src/App.js
+++ book-library/src/App.js
@@ -3,6 +3,23 @@ import Search from "./components/Search";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Database Imports
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDyslQiNsijMCXTzT7eyrNQUkCFIc4I9MI",
+  authDomain: "bookworms-ee96a.firebaseapp.com",
+  projectId: "bookworms-ee96a",
+  storageBucket: "bookworms-ee96a.appspot.com",
+  messagingSenderId: "1058318817192",
+  appId: "1:1058318817192:web:fdc2ad63f4315bf8ceef9d",
+  measurementId: "G-0P65N3JF8T"
+})
+
 
 
 
