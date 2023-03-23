73a86b300dc941c8ba9f56f73f310941ea553bad
Zainab Sebbar <118269226+ZainaSE7835@users.noreply.github.com>
2023-03-23 22:51:37 +0000

    added search box and background image

--- book-library/src/App.js
+++ book-library/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './components/NavbarComp';
+import SearchBar from './components/SearchBar';
 
 
 function App() {
   return (
     <div className='App'>
       <NavbarComp/>
+      <SearchBar placeholder="So, what are we reading today?"/>
     </div>
   );
 }
---
+++ book-library/src/components/SearchBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './SearchBar.css';
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
+
+function SearchBar({ placeholder, data }) {
+  return (
+    <div className="search">
+        <div className="searchInputs">
+            <input type="text" placeholder={placeholder} />
+            <div className="searchIcon">
+            <SearchIcon /></div>
+        </div>
+        <div className="dataResult"></div>
+    </div>
+  )
+}
+
+export default SearchBar
