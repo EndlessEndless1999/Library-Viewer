36b337cccb6374e6abbaa0ef4652d173de32b5ae
Sam Fowora <sffowora@yahoo.co.uk>
2023-03-24 13:53:03 +0000

    App.js update

--- book-library/src/App.js
+++ book-library/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './components/NavbarComp';
 import SearchBar from './components/SearchBar';
+import BookCards from "./components/cards/cards";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <NavbarComp/>
       <SearchBar placeholder="So, what are we reading today?"/>
       <Search />
+      <BookCards />
     </div>
   );
 }
