0e7f6a1fbc595f1b5382ce742ffbd3de2270097e
Sam Fowora <sffowora@yahoo.co.uk>
2023-03-28 12:31:57 +0100

    aligned cards + cleaned up code

--- book-library/src/components/cards/cards.js
+++ book-library/src/components/cards/cards.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import './cards.css';
-import {Card, Image, Heading, Flex} from 'rebass';
+import {Card, Image, Heading, Flex, Box} from 'rebass';
 // import App from '../../App';
 
 const BookCards =({books}) => {
     console.log(books);
     return(
-        <div>
+        <div className='bookArea'>
         {books && books.map((book) => {         
             let bookInfo = book.volumeInfo;
             if ((bookInfo.imageLinks !== undefined) && (bookInfo.imageLinks.smallThumbnail !== undefined)) {
                 return (
-                    <div className="card">
+                    <div className='card'>
                         <Flex>
-                        <Card 
-                        p={3}
-                        width={256}
-                        color='black'
-                        >
+                        <Box>
+                        <Card>
                             <Image src={bookInfo.imageLinks.smallThumbnail} />
                             <Heading>{bookInfo.title}</Heading>
                             <p>{bookInfo.description}</p>
                             {/* Author: {bookInfo.authors.join(", ")}<br />
                             Google Book Link: <a href={bookInfo.infoLink}>{bookInfo.infoLink}</a><br /> */}
                         </Card>
+                        </Box>
                         </Flex>
                     </div>
                 )
@@ -31,7 +29,7 @@ const BookCards =({books}) => {
             return null;
             }
         })}       
-         </div>
+        </div>
     )
 }
 
